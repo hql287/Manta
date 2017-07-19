122be210904e6bd4489e51edd7aea73d2f480b5c
Quốc Hưng Lê <lequochung@me.com>
2017-07-19 16:29:28 +0700

    First Commit

---
+++ app/components/App.jsx
@@ -0,0 +1,11 @@
+// Libraries
+import React from 'react';
+
+// Components
+const App = () => (
+  <div className="header">
+    <h1>Hello, World</h1>
+  </div>
+);
+
+export default App;
---
+++ app/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+// Rendering
+ReactDOM.render( <App />, document.getElementById('root') );
