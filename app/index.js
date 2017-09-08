cb74488810ee4a761990eb8e061e53c80fbe80d4
Quốc Hưng Lê <lequochung@me.com>
2017-09-08 20:09:53 +0700

    Add Measure Middleware

--- app/index.js
+++ app/index.js
@@ -1,44 +1,42 @@
 // Libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-// Redux Stuff
 import {createStore, applyMiddleware, compose} from 'redux';
 import {Provider} from 'react-redux';
-
-// Middleware allows action creator to return a function instead of plain object
-import ReduxThunk from 'redux-thunk';
-import ContactsMW from './middlewares/ContactsMW';
-import InvoicesMW from './middlewares/InvoicesMW';
-import SettingsMW from './middlewares/SettingsMW';
-
-// Log Redux action inside console
-import Logger from 'redux-logger';
 
 // Root Reducer
 import combineReducers from './reducers';
 
-// Integrate Redux Devtool
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-// Create Store
-const AppStore = createStore(
-  combineReducers,
-  composeEnhancers(
-    applyMiddleware(
-      ContactsMW,
-      InvoicesMW,
-      SettingsMW,
-      ReduxThunk,
-      Logger
-    )
-  )
-);
-
-// Main App
+// Root Component
 import App from './App';
 
-// Render App
+// Middleware
+import ReduxThunk from 'redux-thunk';
+import Logger from 'redux-logger';
+import ContactsMW from './middlewares/ContactsMW';
+import InvoicesMW from './middlewares/InvoicesMW';
+import SettingsMW from './middlewares/SettingsMW';
+import MeasureMW from './middlewares/MeasureMW';
+
+const middlewares = [
+  MeasureMW,
+  ContactsMW,
+  InvoicesMW,
+  SettingsMW,
+  ReduxThunk,
+  Logger,
+];
+
+// Redux Devtool
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// Store
+const AppStore = createStore(
+  combineReducers,
+  composeEnhancers(applyMiddleware(...middlewares))
+);
+
+// Render
 ReactDOM.render(
   <Provider store={AppStore}>
     <App />
