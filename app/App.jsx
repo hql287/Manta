4211f99fc7fbda3c5470a063c4028c538aa361c4
Quốc Hưng Lê <lequochung@me.com>
2017-09-15 11:41:56 +0700

    Make App Menu

--- app/App.jsx
+++ app/App.jsx
@@ -6,6 +6,7 @@ const ipc = require('electron').ipcRenderer;
 
 // Actions
 import * as UIActions from './actions/ui';
+import * as FormActions from './actions/form';
 import * as SettingsActions from './actions/settings';
 import * as InvoicesActions from './actions/invoices';
 import * as ContactsActions from './actions/contacts';
@@ -25,13 +26,39 @@ class App extends Component {
   }
 
   componentDidMount() {
-    // Get All Data
     const {dispatch} = this.props;
+    // Get All Data
     dispatch(SettingsActions.getInitalSettings());
     dispatch(ContactsActions.getAllContacts());
     dispatch(InvoicesActions.getInvoices());
     // Add Event Listener
-    ipc.on('menu-change-tab', (event, tabName) => this.changeTab(tabName));
+    ipc.on('menu-change-tab', (event, tabName) => {
+      this.changeTab(tabName);
+    });
+    ipc.on('menu-invoice-save', event => {
+      dispatch(FormActions.saveFormData());
+    });
+    ipc.on('menu-invoice-clear', event => {
+      dispatch(FormActions.clearForm());
+    });
+    ipc.on('menu-invoice-add-item', event => {
+      dispatch(FormActions.addItem());
+    });
+    ipc.on('menu-invoice-toggle-dueDate', event => {
+      dispatch(FormActions.toggleField('dueDate'));
+    });
+    ipc.on('menu-invoice-toggle-currency', event => {
+      dispatch(FormActions.toggleField('currency'));
+    });
+    ipc.on('menu-invoice-toggle-vat', event => {
+      dispatch(FormActions.toggleField('vat'));
+    });
+    ipc.on('menu-invoice-toggle-discount', event => {
+      dispatch(FormActions.toggleField('discount'));
+    });
+    ipc.on('menu-invoice-toggle-note', event => {
+      dispatch(FormActions.toggleField('note'));
+    });
   }
 
   shouldComponentUpdate(nextProps) {
@@ -42,7 +69,17 @@ class App extends Component {
   }
 
   componentWillUnmount() {
-    ipc.removeAllListeners(['menu-change-tab']);
+    ipc.removeAllListeners([
+      'menu-change-tab',
+      'menu-invoice-save',
+      'menu-invoice-clear',
+      'menu-invoice-add-item',
+      'menu-invoice-toggle-dueDate',
+      'menu-invoice-toggle-currency',
+      'menu-invoice-toggle-discount',
+      'menu-invoice-toggle-vat',
+      'menu-invoice-toggle-note',
+    ]);
   }
 
   changeTab(tabName) {
