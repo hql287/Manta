7d6dead887988a01e2658abae2063bedb2e6e86c
ayhankuru <ayhankuru@yandex.com.tr>
2017-12-10 22:32:14 +0300

    Fix Linux Draggable Window

--- libs/dragNdrop.js
+++ libs/dragNdrop.js
@@ -1,3 +1,12 @@
 // Prevent window to open dropped file
 document.addEventListener('dragover', event => event.preventDefault());
 document.addEventListener('drop', event => event.preventDefault());
+
+
+// Linux draggable window broken, so monkey patch 
+if(process.platform == 'linux'){
+    let elements = document.querySelectorAll("body,#root,.appWrapper")
+    elements.forEach(element => {
+        element.classList.add('non-draggable')
+    });
+}
