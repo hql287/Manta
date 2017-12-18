0f327e0bdbb0e408f614bb6221ba3e4439cd1eb3
Hung Q. Le <lequochung@me.com>
2017-12-18 15:37:57 +0700

    Added SplitButton Component

---
+++ app/components/shared/SplitButton.jsx
@@ -0,0 +1,171 @@
+// Libraries
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+// Styles
+import styled from 'styled-components';
+
+const Wrapper = styled.div`position: relative;`;
+
+const ButtonsGroup = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  > a {
+    flex: 1;
+    border: 1px solid rgba(0, 0, 0, 0.1);
+    display: flex;
+    justify-content: center;
+    // align-items: center;
+    padding: 4px 0;
+    font-size: 12px;
+    text-decoration: none;
+    background: #ffffff;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    color: #000;
+    &:first-child {
+      border-radius: 4px 0 0 4px;
+      padding: 4px 12px;
+      text-align: left;
+      justify-content: flext-start;
+      // align-items: flext-start;
+    }
+    &:last-child {
+      border-radius: 0 4px 4px 0;
+      border-left: none;
+      max-width: 24px;
+    }
+  }
+`;
+
+const Addon = styled.div`
+  &:before {
+    content: ' ';
+    height: 10px;
+    width: 10px;
+    position: absolute;
+    top: -6px;
+    right: 10px;
+    transform: rotate(45deg);
+    background: white;
+    border-top: 1px solid rgba(0, 0, 0, 0.1);
+    border-left: 1px solid rgba(0, 0, 0, 0.1);
+  }
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  right: 0;
+  top: 42px;
+  width: 100%;
+  background: white;
+  border-radius: 4px;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  z-index: +999;
+  a {
+    text-transform: uppercase;
+    font-size: 12px;
+    letter-spacing: 1px;
+    padding: 6px 12px;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+    color: black;
+    width: 100%;
+    text-align: left;
+    z-index: 10;
+    &:hover {
+      text-decoration: none;
+      background: #f9fafa;
+    }
+    &:last-child {
+      border-bottom: none;
+    }
+  }
+`;
+
+class SplitButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {showOptions: false};
+    this.handleMainAction    = this.handleMainAction.bind(this);
+    this.handleClick        = this.handleClick.bind(this);
+    this.handleOutsideClick = this.handleOutsideClick.bind(this);
+    this.renderListItems    = this.renderListItems.bind(this);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.showOptions !== nextState.showOptions) {
+      return true;
+    }
+    return false;
+  }
+
+  handleClick() {
+    if (!this.state.showOptions) {
+      document.addEventListener('click', this.handleOutsideClick, false);
+    } else {
+      document.removeEventListener('click', this.handleOutsideClick, false);
+    }
+    this.setState({showOptions: !this.state.showOptions});
+  }
+
+  handleOutsideClick(e) {
+    if (this.node.contains(e.target)) {
+      return;
+    }
+    this.handleClick();
+  }
+
+  handleMainAction(e) {
+    this.setState({showOptions: false}, () => {
+      this.props.mainButton.action();
+    });
+  }
+
+  renderListItems() {
+    const {options} = this.props;
+    return options.map(option => (
+      <a
+        key={option.label.toString()}
+        href="#"
+        onClick={() => {
+          option.action();
+          this.handleClick();
+        }}>
+        {option.label}
+      </a>
+    ));
+  }
+
+  render() {
+    return (
+      <div ref={node => this.node = node}>
+        <Wrapper>
+          <ButtonsGroup>
+            <a href="#" onClick={this.handleMainAction}>
+              {this.props.mainButton.label}
+            </a>
+            <a href="#" onClick={this.handleClick}>
+              <i className="ion-arrow-down-b" />
+            </a>
+          </ButtonsGroup>
+          {this.state.showOptions && <Addon>{this.renderListItems()}</Addon>}
+        </Wrapper>
+      </div>
+    );
+  }
+}
+
+SplitButton.propTypes = {
+  mainButton: PropTypes.shape({
+    label: PropTypes.string.isRequired,
+    action: PropTypes.func.isRequired,
+  }).isRequired,
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string.isRequired,
+      action: PropTypes.func.isRequired,
+    }),
+  ).isRequired,
+};
+
+export default SplitButton;
---
+++ app/components/shared/_tests_/SplitButton.spec.js
@@ -0,0 +1,121 @@
+// Libs
+import React from 'react';
+import {shallow, mount} from 'enzyme';
+import renderer from 'react-test-renderer';
+
+// Component
+import SplitButton from '../SplitButton';
+
+// Mocks
+const mainAction = jest.fn();
+const mainButton = {
+  label: 'Main Action',
+  action: mainAction,
+};
+
+const optionAction1 = jest.fn();
+const optionAction2 = jest.fn();
+const optionAction3 = jest.fn();
+const options = [
+  {
+    label: 'Option A',
+    action: optionAction1,
+  },
+  {
+    label: 'Option B',
+    action: optionAction2,
+  },
+  {
+    label: 'Option C',
+    action: optionAction3,
+  }
+];
+
+describe('Renders correctly to the DOM', () => {
+  let wrapper, toggleBtn, mainBtn;
+  beforeEach(() => {
+    wrapper = shallow(
+      <SplitButton mainButton={mainButton} options={options} />
+    );
+    mainBtn = wrapper.find('a').first();
+    toggleBtn = wrapper.find('a').last();
+  });
+
+  it('renders links as a element', () => {
+    expect(wrapper.find('a')).toHaveLength(2);
+    expect(wrapper.find('button')).toHaveLength(0);
+  });
+
+  it('hide options by default', () => {
+    expect(wrapper.state().showOptions).toEqual(false);
+    expect(wrapper.find('SplitButton__Addon')).toHaveLength(0);
+  });
+
+  it('renders options list correctly', () => {
+    toggleBtn.simulate('click');
+    expect(wrapper.state().showOptions).toEqual(true);
+    expect(wrapper.find('SplitButton__Addon')).toHaveLength(1);
+    expect(
+      wrapper
+      .find('SplitButton__Addon')
+      .children()
+    ).toHaveLength(
+      options.length
+    );
+  });
+
+  it('matches snapshot', () => {
+    const tree = renderer
+      .create(
+        <SplitButton mainButton={mainButton} options={options} />
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Handles click correctly', () => {
+  let wrapper,
+      toggleBtn,
+      mainBtn,
+      spyHandleClick,
+      spyHandleOutsideClick;
+  beforeEach(() => {
+    spyHandleClick = jest.spyOn(SplitButton.prototype, 'handleClick');
+    spyHandleOutsideClick = jest.spyOn(SplitButton.prototype, 'handleOutsideClick');
+    wrapper = shallow(
+      <SplitButton mainButton={mainButton} options={options} />,
+    );
+    mainBtn = wrapper.find('a').first();
+    toggleBtn = wrapper.find('a').last();
+  });
+
+  it('handle main button click correcrtly', () => {
+    mainBtn.simulate('click');
+    expect(mainAction).toHaveBeenCalled();
+  });
+
+  it('handle options click correcrtly', () => {
+    // Show the dropdown
+    toggleBtn.simulate('click');
+    const firstBtn = wrapper.find('SplitButton__Addon').find('a').first();
+    const lastBtn = wrapper.find('SplitButton__Addon').find('a').last();
+    // Click the first btn
+    firstBtn.simulate('click');
+    expect(optionAction1).toHaveBeenCalled();
+    // Click the second btn
+    lastBtn.simulate('click');
+    expect(optionAction3).toHaveBeenCalled();
+  });
+
+  it('handle click outside correctly', () => {
+    // Show the dropdown
+    toggleBtn.simulate('click');
+    // Click the main button - outside the option
+    mainBtn.simulate('click');
+    // The dropdown will be removed
+    expect(wrapper.state().showOptions).toEqual(false);
+    expect(wrapper.find('SplitButton__Addon')).toHaveLength(0);
+  });
+});
+
