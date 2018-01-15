2c718278e0cbcd755833dd5b08e57d95ce2320f0
AlphaStyle <torbjorn1991@hotmail.com>
2018-01-15 16:16:21 +0100

    remove unnecessary if/else statement

--- app/helpers/center-on-primary-display.js
+++ app/helpers/center-on-primary-display.js
@@ -4,22 +4,14 @@ const centerOnPrimaryDisplay = (width, height) => {
   // Get Primary Display (screen / monitor)
   const primaryDisplay = electron.screen.getPrimaryDisplay();
 
-  let x, y;
-
   // TODO: how to do error handling in electron?
   if (!width || !height) {
     throw new Error('missing width and / or height!');
   }
 
-  // If monitor 1 is primary display
-  if (primaryDisplay.bounds.x === 0 && primaryDisplay.bounds.y === 0) {
-    x = primaryDisplay.bounds.width / 2 - width / 2;
-    y = primaryDisplay.bounds.height / 2 - height / 2;
-  } else {
-    // if monitor 2 and higher is primary display
-    x = primaryDisplay.bounds.width / 2 - width / 2 + primaryDisplay.bounds.x;
-    y = primaryDisplay.bounds.height / 2 - height / 2 + primaryDisplay.bounds.y;
-  }
+  // X and Y coordinates to make rectangular center of primary display
+  const x = primaryDisplay.bounds.width / 2 - width / 2 + primaryDisplay.bounds.x;
+  const y = primaryDisplay.bounds.height / 2 - height / 2 + primaryDisplay.bounds.y;
 
   return { x, y };
 };
