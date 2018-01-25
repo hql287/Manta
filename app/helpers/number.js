dd3d16e8c9fa908ef467eca0a6705f387c5af7f8
DarkSmile92 <robin.kretzschmar@yahoo.de>
2018-01-25 13:51:58 +0100

    Closes hql287/Manta#190, Closes hql287/Manta#173

--- app/helpers/number.js
+++ app/helpers/number.js
@@ -1,11 +1,19 @@
+const appConfig = require('electron').remote.require('electron-settings');
+
 function roundValue(value) {
   // This variable my get value from setting in future version
   const decimals = 2;
   return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
 }
 
-function formatNumber(number) {
-  return roundValue(number).toLocaleString();
+function formatNumber(number, locale) {
+  if (!locale) {
+    locale = appConfig.get('general.language') || 'en';
+  }
+  return roundValue(number).toLocaleString(locale, {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+  });
 }
 
 export { formatNumber };
