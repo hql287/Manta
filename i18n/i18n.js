05483fe27f3ea028a0753311e2ea117c9b1962d1
Hung Q. Le <lequochung@me.com>
2018-01-29 11:13:47 +0700

    Keep i18n with moment in sync

--- i18n/i18n.js
+++ i18n/i18n.js
@@ -1,34 +1,45 @@
 import i18n from 'i18next';
 import { reactI18nextModule } from 'react-i18next';
 
-const isDev = require('electron-is-dev');
 const appConfig = require('electron').remote.require('electron-settings');
 const defaultLanguage = appConfig.get('general.language');
+const isDev = require('electron-is-dev');
+const moment = require('moment');
 
 // Language Files
 import fr from './fr/index.js';
 import en from './en/index.js';
 import vi from './vi/index.js';
 
-i18n
-  .use(reactI18nextModule)
-  .init({
-    lng: defaultLanguage,
-    fallbackLng: 'en',
-    debug: isDev,
-    // ns: ['form', 'common', 'invoices'],
-    defaultNS: 'form',
-    resources: {
-      en,
-      fr,
-      vi,
+i18n.use(reactI18nextModule).init({
+  lng: defaultLanguage,
+  fallbackLng: 'en',
+  debug: isDev,
+  // ns: ['form', 'common', 'invoices'],
+  defaultNS: 'form',
+  resources: {
+    en,
+    fr,
+    vi,
+  },
+  interpolation: {
+    function(value, format, lng) {
+      if (format === 'uppercase') return value.toUpperCase();
+      if (value instanceof Date) return moment(value).format(format);
+      return value;
     },
-    react: {
-      wait: false,
-      bindI18n: 'languageChanged loaded',
-      bindStore: 'added removed',
-      nsMode: 'default'
-    }
-  });
+  },
+  react: {
+    wait: false,
+    bindI18n: 'languageChanged loaded',
+    bindStore: 'added removed',
+    nsMode: 'default',
+  },
+});
+
+// Keep the language on moment in sync with i18next
+i18n.on('languageChanged', lang => {
+  moment.locale(lang);
+});
 
 export default i18n;
