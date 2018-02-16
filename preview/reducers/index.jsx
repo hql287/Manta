f4d230fcfe16926ef3bcf3592b9b1f192e630447
Hung Q. Le <lequochung@me.com>
2018-02-16 23:06:54 +0700

    Only notify previewWindow when the UI language changes
    Also added action and update reducer to handle this change

--- preview/reducers/index.jsx
+++ preview/reducers/index.jsx
@@ -1,25 +1,31 @@
-import * as ACTION_TYPES from '../constants/actions.jsx';
+// Libs
+import * as ACTION_TYPES from '../constants/actions';
 import { createSelector } from 'reselect';
 import { handleActions } from 'redux-actions';
 const appConfig = require('electron').remote.require('electron-settings');
 const invoiceSettings = appConfig.get('invoice');
-const profile = appConfig.get('profile');
+const profileSettings = appConfig.get('profile');
+const generalSettings = appConfig.get('general');
 
 const initialState = {
+  ui: { language: generalSettings.language },
   invoice: {},
-  profile,
+  profile: profileSettings,
   configs: {
     accentColor: {
       useCustom: false,
       color: '#2CCCE4',
     },
+    // Set default from settings
+    dateFormat: invoiceSettings.dateFormat,
+    language: generalSettings.language,
+    template: invoiceSettings.template,
+    // Other settings
     alignItems: 'middle',
     fontSize: '200',
     showLogo: true,
     showRecipient: true,
     useSymbol: true,
-    template: invoiceSettings.template,
-    dateFormat: invoiceSettings.dateFormat,
   },
 };
 
@@ -28,6 +34,13 @@ const RootReducer = handleActions(
     [ACTION_TYPES.INVOICE_UPDATE]: (state, action) =>
       Object.assign({}, state, {
         invoice: action.payload,
+        configs: action.payload.configs ? action.payload.configs : state.configs
+      }),
+    [ACTION_TYPES.UI_CHANGE_LANGUAGE]: (state, action) =>
+      Object.assign({}, state, {
+        ui: Object.assign({}, state.ui, {
+          language: action.payload
+        })
       }),
     [ACTION_TYPES.SETTINGS_UPDATE_CONFIGS]: (state, action) =>
       Object.assign({}, state, {
@@ -40,6 +53,7 @@ const RootReducer = handleActions(
       return Object.assign({}, state, {
         profile,
         configs: Object.assign({}, state.configs, {
+          language: general.language,
           template: invoice.template,
           dateFormat: invoice.dateFormat,
         }),
@@ -56,3 +70,6 @@ const getState = state => state;
 export const getConfigs = createSelector(getState, state => state.configs);
 export const getInvoice = createSelector(getState, state => state.invoice);
 export const getProfile = createSelector(getState, state => state.profile);
+export const getUILang  = createSelector(getState, state => state.ui.language);
+export const getInvoiceLang  = createSelector(getState, state => state.invoice.language);
+
