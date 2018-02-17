9847282e0c21221d138d40a8424cea3366fc3dcb
Hung Q. Le <lequochung@me.com>
2018-02-17 12:19:12 +0700

    Fixed action buttons spacing

--- preview/components/sidebar/Actions.jsx
+++ preview/components/sidebar/Actions.jsx
@@ -4,6 +4,18 @@ import PropTypes from 'prop-types';
 
 import { Section, Label } from '../shared';
 import Button from '../../../app/components/shared/Button';
+import styled from 'styled-components';
+
+const ButtonsGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  > * {
+    margin-bottom: 10px;
+    &:last-child {
+    margin-bottom: 0;
+    }
+ }
+`;
 
 function Actions({ saveConfigs, savePDF, t, UILang }) {
   return (
@@ -11,12 +23,14 @@ function Actions({ saveConfigs, savePDF, t, UILang }) {
       <Label>
         {t('preview:sidebar:actions:name', { lng: UILang })}
       </Label>
-      <Button block primary onClick={savePDF}>
-        {t('preview:sidebar:actions:savePDF', { lng: UILang })}
-      </Button>
-      <Button block secondary onClick={saveConfigs}>
-        {t('preview:sidebar:actions:saveConfigs', { lng: UILang })}
-      </Button>
+      <ButtonsGroup>
+        <Button block primary onClick={savePDF}>
+          {t('preview:sidebar:actions:savePDF', { lng: UILang })}
+        </Button>
+        <Button block secondary onClick={saveConfigs}>
+          {t('preview:sidebar:actions:saveConfigs', { lng: UILang })}
+        </Button>
+      </ButtonsGroup>
     </Section>
   );
 }
