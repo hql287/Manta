b124dfdc9db2f757b065ff543bf0e8bacc63685c
jens <kontakt@jenstangermann.com>
2018-03-02 16:15:45 +0100

    added info about devtools

--- app.js
+++ app.js
@@ -154,6 +154,8 @@ function createPreviewWindow() {
 }
 
 function addDevToolsExtension() {
+  if (process.env.DEVTRON_DEV_TOOLS_PATH)
+    BrowserWindow.addDevToolsExtension(process.env.DEVTRON_DEV_TOOLS_PATH);
   if (process.env.REACT_DEV_TOOLS_PATH)
     BrowserWindow.addDevToolsExtension(process.env.REACT_DEV_TOOLS_PATH);
   if (process.env.REDUX_DEV_TOOLS_PATH)
