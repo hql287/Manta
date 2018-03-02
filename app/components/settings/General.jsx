9f66bfa5ff74396611edb06529c653c4e50995cc
Hung Q. Le <lequochung@me.com>
2018-03-02 18:39:10 +0700

    Loaded new languages and updated dropdown lists in Settings and Preview

--- app/components/settings/General.jsx
+++ app/components/settings/General.jsx
@@ -99,7 +99,7 @@ class General extends Component {
                 <option value="de">{t('settings:fields:language:de', { lng: 'de' })}</option>
                 <option value="en">{t('settings:fields:language:en', { lng: 'en' })}</option>
                 <option value="fr">{t('settings:fields:language:fr', { lng: 'fr' })}</option>
-                <option value="ko">{t('settings:fields:language:ko', { lng: 'ko' })}</option>
+                <option value="id">{t('settings:fields:language:id', { lng: 'id' })}</option>
                 <option value="vi">{t('settings:fields:language:vi', { lng: 'vi' })}</option>
                 <option value="zh-CN">{t('settings:fields:language:zh-CN', { lng: 'zh-CN' })}</option>
               </select>
--- preview/components/sidebar/Language.jsx
+++ preview/components/sidebar/Language.jsx
@@ -13,7 +13,7 @@ function Language({ t, language, handleInputChange, UILang }) {
         <option value="de">{t('settings:fields:language:de', { lng: 'de' })}</option>
         <option value="en">{t('settings:fields:language:en', { lng: 'en' })}</option>
         <option value="fr">{t('settings:fields:language:fr', { lng: 'fr' })}</option>
-        <option value="ko">{t('settings:fields:language:ko', { lng: 'ko' })}</option>
+        <option value="id">{t('settings:fields:language:id', { lng: 'id' })}</option>
         <option value="vi">{t('settings:fields:language:vi', { lng: 'vi' })}</option>
         <option value="zh-CN">{t('settings:fields:language:zh-CN', { lng: 'zh-CN' })}</option>
       </select>
