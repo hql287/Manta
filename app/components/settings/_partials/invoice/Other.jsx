c053f05aa2426fe729c0e69fd72d50235f81600f
jens <kontakt@jenstangermann.com>
2018-03-04 01:03:41 +0100

    added more german date formats

--- app/components/settings/_partials/invoice/Other.jsx
+++ app/components/settings/_partials/invoice/Other.jsx
@@ -79,8 +79,17 @@ function Other({
             <option value="DD/MM/YY">
               {moment(Date.now()).format('DD/MM/YY')} (DD/MM/YY)
             </option>
+            <option value="dddd, Do MMMM YYYY">
+              {moment(Date.now()).format('dddd, Do MMMM YYYY')} (dddd, Do MMMM YYYY)
+            </option>
+            <option value="Do MMMM YYYY">
+              {moment(Date.now()).format('Do MMMM YYYY')} (Do MMMM YYYY)
+            </option>
             <option value="DD.MM.YYYY">
               {moment(Date.now()).format('DD.MM.YYYY')} (DD.MM.YYYY)
+            </option>
+            <option value="DD.MM.YY">
+              {moment(Date.now()).format('DD.MM.YY')} (DD.MM.YY)
             </option>
           </select>
         </Field>
