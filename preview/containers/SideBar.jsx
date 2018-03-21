8132d6cd00a590b57b893734ba56b19b14aba1d1
Jens Tangermann <jens-t@users.noreply.github.com>
2018-03-21 04:58:58 +0100

    Sidebar overflow fixes (#257)
    * overflow fixes
    
    * changed sidebar width to 215px
    
    * sidebar window controls fix for macOS
    
    * reverted the macOS style conditions
    
    for the window controls
    
    * sticky actions
    
    * Changed sticky box background and border
    
    * Refactored Sidebar CSS
    
    * Make sure the fixed element will not cover any other elements
    
    * Fixed Linting Error
    
    * Revert "Fixed Linting Error"
    
    This reverts commit 3766aaf4ffc06717239dc819be65383c2ecfb73f.
    
    * Revert "Make sure the fixed element will not cover any other elements"
    
    This reverts commit 1349ff4a5a5b4244e204cd73e75035b6dcafb6cd.
    
    * Revert "Refactored Sidebar CSS"
    
    This reverts commit 0a1a2c74ad921fb502880ef7811f8a9e0475d3db.

--- preview/containers/SideBar.jsx
+++ preview/containers/SideBar.jsx
@@ -6,20 +6,26 @@ import i18n from '../../i18n/i18n';
 
 // Style
 import styled from 'styled-components';
+const OverflowWrapper = styled.div`
+  min-width: 150px;
+  width: 220px;
+  max-width: 600px;
+  height: 100vh;
+  overflow-y: auto;
+  overflow-x: hidden;
+  justify-content: flex-start;
+  background: #f9fafa;
+  border-right: 1px solid #e0e1e1;
+`;
 const Wrapper = styled.div`
-  flex: 1;
   display: flex;
   flex-direction: column;
-  background: #f9fafa;
-  border-right: 1px solid rgba(0, 0, 0, 0.1);
-  width: 180px;
-  min-width: 180px;
-  max-width: 180px;
+  height: 100%;
+  min-height: min-content;
   padding: 20px;
-  justify-content: flex-start;
+
   > div:last-child {
-    flex: 1;
-    margin-bottom: 0;
+    flex: 1 0 auto;
   }
 `;
 
@@ -81,67 +87,69 @@ class SideBar extends Component {
       template,
     } = configs;
     return (
-      <Wrapper>
-        <Template
-          t={t}
-          UILang={UILang}
-          template={template}
-          handleInputChange={this.handleInputChange}
-        />
-        <Language
-          t={t}
-          UILang={UILang}
-          language={language}
-          handleInputChange={this.handleInputChange}
-        />
-        <DateFormat
-          t={t}
-          UILang={UILang}
-          language={language}
-          dateFormat={dateFormat}
-          handleInputChange={this.handleInputChange}
-        />
-        <Alignment
-          t={t}
-          UILang={UILang}
-          alignItems={alignItems}
-          handleInputChange={this.handleInputChange}
-        />
-        <FontSize
-          t={t}
-          UILang={UILang}
-          fontSize={fontSize}
-          handleInputChange={this.handleInputChange}
-        />
-        { showLogo && (
-          <LogoSize
+      <OverflowWrapper>
+        <Wrapper>
+          <Template
             t={t}
             UILang={UILang}
-            logoSize={logoSize}
+            template={template}
             handleInputChange={this.handleInputChange}
           />
-        )}
-        <Toggler
-          t={t}
-          UILang={UILang}
-          configs={configs}
-          handleInputChange={this.handleInputChange}
-        />
-        { customAccentColor && (
-          <AccentColor
+          <Language
             t={t}
             UILang={UILang}
-            accentColor={accentColor}
-            handleAccentColorChange={this.handleAccentColorChange}
+            language={language}
+            handleInputChange={this.handleInputChange}
           />
-        )}
-        <Actions
-          t={t}
-          UILang={UILang}
-          savePDF={this.savePDF}
-          saveConfigs={this.saveConfigs}
-        />
-      </Wrapper>
+          <DateFormat
+            t={t}
+            UILang={UILang}
+            language={language}
+            dateFormat={dateFormat}
+            handleInputChange={this.handleInputChange}
+          />
+          <Alignment
+            t={t}
+            UILang={UILang}
+            alignItems={alignItems}
+            handleInputChange={this.handleInputChange}
+          />
+          <FontSize
+            t={t}
+            UILang={UILang}
+            fontSize={fontSize}
+            handleInputChange={this.handleInputChange}
+          />
+          { showLogo && (
+            <LogoSize
+              t={t}
+              UILang={UILang}
+              logoSize={logoSize}
+              handleInputChange={this.handleInputChange}
+            />
+          )}
+          <Toggler
+            t={t}
+            UILang={UILang}
+            configs={configs}
+            handleInputChange={this.handleInputChange}
+          />
+          { customAccentColor && (
+            <AccentColor
+              t={t}
+              UILang={UILang}
+              accentColor={accentColor}
+              handleAccentColorChange={this.handleAccentColorChange}
+            />
+          )}
+          <Actions
+            t={t}
+            UILang={UILang}
+            savePDF={this.savePDF}
+            saveConfigs={this.saveConfigs}
+          />
+        </Wrapper>
+      </OverflowWrapper>
     );
   }
 }
