cc5f031d4e9eb3fd07bcf7c8062b909672b6d567
itsMikey <mikey.aburamadan@gmail.com>
2021-01-23 10:13:53 -0800

    make sure remote is ready before calling in centerOnPrimaryDisplay

--- helpers/center-on-primary-display.js
+++ helpers/center-on-primary-display.js
@@ -1,10 +1,17 @@
-const {app, screen} = require('electron');
+const {remote} = require('electron');
 
 const centerOnPrimaryDisplay = (winWidth, winHeight) => {
-  return app.whenReady().then(() => {
-      console.log(screen);
+    const isRemoteReady = remote && remote.app;
+    // on startup, remote is not available. Will be availble during runtime
+    if (!isRemoteReady) {
+        return {
+            x: 0,
+            y: 0
+        }
+    }
+  return remote.app.whenReady().then(() => {
     // Get primary display (screen / monitor) bounds
-    const primaryDisplay = screen.getPrimaryDisplay();
+    const primaryDisplay = remote.screen.getPrimaryDisplay();
     const { x, y, width, height } = primaryDisplay.bounds;
 
     // Calculate X and Y coordinates to make rectangular center on primary display
